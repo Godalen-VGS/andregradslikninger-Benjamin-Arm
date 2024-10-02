44765da93c695963cac6691ff125d471fdeaf083
Benjmain-Arm <benjamin.m.armstrong08@gmail.com>
2024-10-02 10:40:05 +0200

    test

--- andregradslikning.py
+++ andregradslikning.py
@@ -2,5 +2,24 @@
 import math
 
 def losninger(a, b, c):
+  diskriminant = b**2-4*a*c
+  if diskriminant < 0:
+    return "Likningen har ingen løsning"
+  
+  losning1 = round((-b + math.sqrt(diskriminant))/(2*a), 2)
+  losning2 = round((-b - math.sqrt(diskriminant))/(2*a), 2)
+
+  print(diskriminant)
+
+  løsning1 print(diskrimant)
+
+
+  if diskriminant  == 0:
+    return losning1 #Et flyttall avrundet til to desimaler
+  else:
+    return (losning1, losning2)
+  
+
+  
   # Din kode her
   return #Returneringsverdi
